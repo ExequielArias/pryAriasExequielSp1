f8fb6cc6d5dbfd9894978eb23832505bfbb8eb21
root <mgiraudo@ies21.edu.ar>
2023-05-12 12:00:45 -0300

    12/05/23 12:00

--- pryAriasExequielSp1/frmBienvenida.Designer.cs
+++ pryAriasExequielSp1/frmBienvenida.Designer.cs
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(410, 220);
             this.Controls.Add(this.lblBienvenido);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
